3260164290995abb8a3ff2749950151159222bc3
engrvitalis <ndlinks@gmail.com>
2021-04-17 20:10:53 +0100

    get_table function completed

--- achke_ann.py
+++ achke_ann.py
@@ -8,3 +8,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+def get_tables(file1, file2):
+    
+  # Reads in files.
+  dfs = [pd.read_csv(file1), pd.read_csv(file2)]
+
+  # Create dummies.
+  for i, df in enumerate(dfs):
+    dfs[i] = pd.get_dummies(df, drop_first=True)
+
+  # Rearrange columns.
+  dfs[0] = pd.concat([dfs[0].iloc[:, :4], dfs[0].iloc[:, -4:], dfs[0].iloc[:, 4:8]], axis=1)
+  dfs[1] = pd.concat([dfs[1].iloc[:, :4], dfs[1].iloc[:, -3:], dfs[1].iloc[:, 4:9]], axis=1)
+
+  return dfs
+
+file1 = 'welding_data_by_properties.csv'
+file2 = 'welding_data_by_welding_type.csv'
+
