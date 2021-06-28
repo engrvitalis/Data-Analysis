f70547383bfa243d478400405386bd983dc37fcb
engrvitalis <ndlinks@gmail.com>
2021-06-28 23:41:37 +0100

    Successful creation of module

--- elm_ann.py
+++ elm_ann.py
@@ -1,10 +1,8 @@
-"""Import the required modules"""
-from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-from scipy.linalg import pinv2
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import time
+
+def get_file(files):
+    df = []
+    for f in files:
+        df.append(pd.read_csv(f))
+    
+    return df
--- impact_test_analysis.py
+++ impact_test_analysis.py
@@ -1,1 +1,16 @@
-import elm_ann
+"""Import the required modules"""
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from scipy.linalg import pinv2
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+import elm_ann as ea
+
+file = ('impact1.csv', 'impact2.csv')
+df = ea.get_file(file)
+
+print(df[0])
