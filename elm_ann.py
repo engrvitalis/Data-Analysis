b6c9ca2ec19b8f916445c0ea2420309b26d4e097
engrvitalis <ndlinks@gmail.com>
2021-06-30 09:11:49 +0100

    More functions added

--- elm_ann.py
+++ elm_ann.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from scipy.linalg import pinv2
+import numpy as np
+import time
 
 def get_file(files):
     df = []
@@ -62,11 +69,103 @@ def explore(df, ntarget=1):
     plt.show()
     print('\n')
 
-    def stats():
-        for j in range(2):
-            print('\nData Statistical Summary')
-            display(df[x].describe())
-            print('')
+def generate_train_test(df, ntarget=1):
+    X = df.iloc[:, :ntarget]
+    y = df.iloc[:, ntarget]
 
-            print('\nData Visualization')
-            explore(df[x], 4)
+    return train_test_split(X, y,random_state=1, test_size=0.2)
+
+def scale_data(X_train, X_test):
+    sc_X = StandardScaler()
+    X_trainscaled=sc_X.fit_transform(X_train)
+    X_testscaled=sc_X.transform(X_test)
+
+    return X_trainscaled, X_testscaled
+
+def ann_analysis():
+    X_trainscaled, X_testscaled = scale_data(X_train, X_test)
+    reg = MLPRegressor(hidden_layer_sizes=(64, 64, 64), activation='relu', random_state=1, max_iter=5000).fit(X_trainscaled, y_train)
+
+    y_pred=reg.predict(X_testscaled)
+    r2 = r2_score(y_pred, y_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
+
+    return r2, rmse, mae, y_pred, reg
+
+def elm_analysis(hidden_size=5000):
+    input_size = X_train.shape[1]
+    hidden_size = hidden_size
+    input_weights = np.random.normal(size=[input_size,hidden_size])
+    biases = np.random.normal(size=[hidden_size])
+
+    def relu(x):
+        return np.maximum(x, 0, x)
+
+    def hidden_nodes(X):
+        G = np.dot(X, input_weights)
+        G = G + biases
+        H = relu(G)
+        return H
+
+    output_weights = np.dot(pinv2(hidden_nodes(X_train)), y_train)
+
+    def predict(X):
+        out = hidden_nodes(X)
+        out = np.dot(out, output_weights)
+        return out
+
+    y_pred = predict(X_test)
+    r2 = r2_score(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
+
+    return r2, rmse, mae, y_pred
+
+def disp_matrics(r2, rmse, mae, title):
+    print(title)
+    print('===============================================')
+    print(f'R^2:\t\t{r2}')
+    print(f'rmse:\t\t{rmse}')
+    print(f'mae:\t\t{mae}')
+    print(f'time(s)\t\t{stop - start}')
+    print('\n')
+
+def disp_graphs(i, y_test, y_ann_pred, y_elm_pred):
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.scatter(y_ann_pred, y_test)
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=3)
+    plt.title('ANN')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+
+    plt.subplot(1, 2, 2)
+    plt.scatter(y_elm_pred, y_test)
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=3)
+    plt.title('ELM')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+
+    plt.suptitle('Visualizing Models Predictive Ability', x=.506, y=1.03, fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+def disp_compare_graphs():
+    plt.figure(figsize = (10, 5))
+
+    x = np.arange(4)
+    ann = [ann_r2*100, ann_rmse, ann_mae, ann_time]
+    elm = [elm_r2*100, elm_rmse, elm_mae, elm_time]
+    width = 0.40
+
+    # plot data in grouped manner of bar type
+    plt.bar(x-0.2, ann, width, color='green')
+    plt.bar(x+0.2, elm, width, color='red')
+    plt.xticks(x, ['R^2', 'RMSE', 'MAE', 'TIME(S)'])
+    plt.title("Visualizing Models Data Metrics")
+    plt.xlabel("Metrics")
+    plt.ylabel("Values")
+    plt.legend(["ANN", "ELM"])
+    plt.show()
