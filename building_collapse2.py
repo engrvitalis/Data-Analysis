0cd7507e5c8aaa6edc3104ecf594a82627c77f45
engrvitalis <ndlinks@gmail.com>
2021-07-20 19:00:37 +0100

    Algorithmic changes in some files while others are removed

--- building_collapse2.py
+++ building_collapse2.py
@@ -1,10 +1,33 @@
+import random
+
 # Generate test values for buiding collapse prediction
 rating_num = []
-for i in range(10):
-    num = []
-    for j in range(10):
-        x = random.randint(0, 1)
-        num.append(x)
-        print(x, end="")
+count = 30
+three = 0
+two = 0
+one = 0
+while True:
+    for i in range(count):
+        num = []
+        for j in range(9):
+            x = random.randint(0, 1)
+            num.append(x)
+
+        if num[2] or num[5] or num[7]:
+            if three < round(count/3):
+                three += 1
+                num.append(3)
+
+    
+    # if num[1] or num[4] or num[6]:
+    #     num.append(2)
+    
+    # if num[0] or num[3] or num[8]:
+    #     num.append(1)
+    
+
+
+    print(num)
+    
     rating_num.append(num)
-    print('\n')
+    # print('\n')
